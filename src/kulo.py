94e4e0989ed1260fb275c418aab378c518d74dd4
Michael Peters <michaelpeterswa@icloud.com>
2021-01-19 20:28:13 -0800

    feat: parsing GeoJSON into centroid points with the "properties" attached

--- src/kulo.py
+++ src/kulo.py
@@ -2,6 +2,7 @@
 # kulo.py
 
 import geojson
+from shapely.geometry import shape, MultiPolygon, Polygon, Point
 
 input_file = "..\data\Washington_Large_Fires_1973-2019.geojson"
 
@@ -14,12 +15,50 @@ def loadData(filename):
         data = geojson.load(f)
     return data
 
+def returnAcreage(fire):
+    """
+    return acreage of a fire
+    """
+    return fire["properties"]["ACRES"]
+
+def createPolygon(fire):
+    """
+    create a Polygon object from list of points
+    """
+    points = []
+    for coordinate in fire["geometry"]["coordinates"][0]:
+        points.append(tuple(coordinate))
+    polygon = Polygon(points)
+    return polygon
+
+def createPolygonFromMulti(fire):
+    """
+    https://gis.stackexchange.com/questions/166934/python-library-for-converting-geojson-multi-polygon-to-polygon
+    """
+    multipolygon = [x.buffer(0) for x in shape(fire["geometry"]).buffer(0).geoms]
+    max_poly = max(multipolygon, key=lambda a: a.area)
+    return max_poly
+
+def generateCentroid(polygon):
+    """
+    calculate and return centroid of a polygon
+    """
+    return polygon.centroid.coords
+
+def isMultiPolygonal(fire):
+    """
+    return true if the object is a MultiPolygon
+    """
+    return True if fire["geometry"]["type"] == "MultiPolygon" else False
+    
+
+
 if __name__ == "__main__":
-    max_acres = 0
     fire_data = loadData(input_file)
     fire_data = fire_data["features"]
+    results = []
     for fire in fire_data:
-        acres = fire["properties"]["ACRES"]
-        if acres > max_acres:
-            max_acres = acres
-    print(max_acres)
+        poly = createPolygonFromMulti(fire) if isMultiPolygonal(fire) else createPolygon(fire)
+        fire_centroid = generateCentroid(poly)
+        results.append((list(fire_centroid), fire["properties"]))
+    print(results)
